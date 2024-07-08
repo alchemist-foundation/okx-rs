a94b10235a092535d382717eda74563a8e0f3327
Thanatorn.K <tk.thanatorn@gmail.com>
2024-07-08 14:36:48 +0700

    impl: get instruments api

--- src/api/v5/model.rs
+++ src/api/v5/model.rs
@@ -1,4 +1,4 @@
-use crate::serde_util::*;
+use crate::{impl_string_enum, serde_util::*};
 use serde::{Deserialize, Serialize};
 
 use crate::serde_util::MaybeU64;
@@ -9,3 +9,22 @@ pub struct OKXSystemTime {
     #[serde(default, with = "str_opt")]
     pub ts: MaybeU64,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum InstrumentType {
+    Spot,
+    Margin,
+    Swap,
+    Futures,
+    Option,
+    Any,
+}
+
+impl_string_enum!(InstrumentType,
+    Spot => "SPOT",
+    Margin => "MARGIN",
+    Swap => "SWAP",
+    Futures => "FUTURES",
+    Option => "OPTION",
+    Any => "ANY",
+);
--- src/api/v5/public_data.rs
+++ src/api/v5/public_data.rs
@@ -5,9 +5,21 @@ use crate::api::v5::{model::OKXSystemTime, Request};
 
 pub mod rest {
 
+    use crate::api::v5::model::InstrumentType;
+
     use super::*;
 
+    /// https://www.okx.com/docs-v5/en/?shell#public-data-rest-api-get-system-time
+    /// ## Get System Time
+    /// Retrieve API server time.
+    ///
+    /// Rate Limit: 10 requests per 2 seconds <br/>
+    /// Rate limit rule: IP
+    ///
+    /// ### HTTP Request
+    /// [GET] /api/v5/public/time
     #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
     pub struct GetSystemTime;
     impl Request for GetSystemTime {
         const METHOD: Method = Method::GET;
@@ -15,5 +27,30 @@ pub mod rest {
         const AUTH: bool = false;
 
         type Response = Vec<OKXSystemTime>;
+    }
+
+    /// https://www.okx.com/docs-v5/en/#public-data-rest-api-get-instruments
+    /// ## Get instruments
+    /// Retrieve a list of instruments with open contracts.
+    ///
+    /// Rate Limit: 20 requests per 2 seconds <br/>
+    /// Rate limit rule: IP + instrumentType
+    ///
+    /// ### HTTP Request
+    /// [GET] /api/v5/public/instruments
+    #[derive(Debug, Clone, Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct GetInstruments {
+        /// Instrument Type: SPOT, MARGIN, SWAP, FUTURES, OPTION
+        pub inst_type: InstrumentType,
+
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub uly: Option<String>,
+
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub inst_family: Option<String>,
+
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub inst_id: Option<String>,
     }
 }
