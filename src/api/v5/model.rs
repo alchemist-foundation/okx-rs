022514b619c0c102a04adff8de06a4e61cd851cf
Thanatorn.K <tk.thanatorn@gmail.com>
2024-07-08 13:30:44 +0700

    try: convert at serialize

--- src/api/v5/model.rs
+++ src/api/v5/model.rs
@@ -1,7 +1,11 @@
+use crate::serde_util::*;
 use serde::{Deserialize, Serialize};
+
+use crate::serde_util::MaybeU64;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct OKXSystemTime {
-    pub ts: u64,
+    #[serde(default, with = "str_opt")]
+    pub ts: MaybeU64,
 }
